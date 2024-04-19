f270a48cf2fea64cf5bd0fdaae4279dc080c1d8d
Philip <philipjanik3@gmail.com>
2024-04-18 19:11:01 -0600

    routing+pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
+  { path: '', redirectTo: 'home-component', pathMatch: 'full' },
+  { path: 'contact-component', component: ContactComponent },
+  { path: 'projects-component', component: ProjectsComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
