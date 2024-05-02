0772963eeed28c6f0115f9514d196f46e786cdb7
Philip <philipjanik3@gmail.com>
2024-05-02 13:27:27 -0600

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,8 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     RouterOutlet, 
     RouterLink, 
     RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [MatExpansionModule],
+  imports: [
+    MatExpansionModule,
+    MatListModule,
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
